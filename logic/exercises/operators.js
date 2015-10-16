7b95016db19fa2636dbba69ffe41eb0577210072
John Ochs <johnochs@me.com>
2015-10-16 04:46:06 -0700

    Breaks up operators from coercion and adds exercises to their own folder.  Starts on coercion and truthy/falsy

---
+++ logic/exercises/operators.js
@@ -0,0 +1,24 @@
+// One by one, execute each line below.  Note the results.  These lines are
+// (believe it or not) actually curated so that if you're paying attention,
+// you should see some stuff that will just not make sense.  Save these
+// questions and we will discuss them in person.
+
+"5" == 5;
+"5" === 5;
+true == "2";
+true == "1";
+true == 1;
+true === 1;
+false != "false";
+0 == "";
+[] == ![];
+false == undefined;
+false == null;
+null == undefined;
+
+//And just for kicks, try defining
+var a = {};
+
+//Now try this:
+
+a == '[object Object]';
